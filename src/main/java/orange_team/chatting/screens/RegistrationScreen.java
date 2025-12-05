9a1f8c1e3423e8dddc7469cc1750bb22d771ba5c
developer1 <developer1>
2025-12-05 02:11:42 -0300

    Continuo con los registros la proxima parte ya debera incluir Base de datos

--- src/main/java/orange_team/chatting/screens/RegistrationScreen.java
+++ src/main/java/orange_team/chatting/screens/RegistrationScreen.java
@@ -1,14 +1,46 @@
 package orange_team.chatting.screens;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import java.net.http.HttpRequest;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+//pantalla dedicada a recolectar informacion necesaria para registrar a un usuario
+//Aqui tenemos que recibir tanto la peticion con form inicial, como 
+//tambien redirigir al usuario dependiendo del resultado de la operacion
+//La operacion debera de redirigirme a una pantalla de error explicando el motivo de la falla
+//tambien a una pantalla de exito que me redirija a mi menu de usuario
 @Controller
+@RequestMapping("Registration")
 public class RegistrationScreen {
-    @GetMapping("/registration")
-    public String GetScreen()
+    static String _index = "registration_screen/index.html";
+    static String _succes_index = "registration_succes/index.html";
+    @RequestMapping(method = {RequestMethod.GET},
+                    value = "/")
+    public String GetScreen(HttpRequest request, ModelAndView mv)
     {
-        return "registration_screen/index.html";
+
+        //Whe leave this like that so I can do things as to add translations later
+        return _index;
     }
-    
+
+    @ResponseBody
+    @RequestMapping(
+        value = "/SuccesfullRegistration",
+        method = RequestMethod.POST//a esta seccion solo se puede acceder atravez de post methods
+    )
+    public ModelAndView GetSuccesScreen(ModelAndView mv)
+    {
+        return mv;
+    }
+    @ResponseBody
+    @RequestMapping(
+        value = "/FailureRegisteringUser"
+    )
+    public ModelAndView GetFailureScreen(ModelAndView mv)
+    {
+        return mv;
+    }
 }
